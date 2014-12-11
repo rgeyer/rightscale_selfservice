cf56bd05ebed4b8d0aa9f46f1415b4d1723daf51
Ryan J. Geyer <me@ryangeyer.com>
2014-12-11 11:50:43 -0800

    Even MORE explicit inclusion of base, to satisfy Travis

--- lib/rightscale_selfservice/cli/execution.rb
+++ lib/rightscale_selfservice/cli/execution.rb
@@ -17,6 +17,9 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+# Require base.rb first, to satisfy Travis CI
+require File.expand_path(File.join(File.dirname(__FILE__), 'base'))
+
 module RightScaleSelfService
   module Cli
     class Execution < Base
